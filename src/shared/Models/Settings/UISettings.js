4219be1fc892d57bb28a6d53dea75c7b14789526
Thomas Beverley <tom@locly.com>
2017-04-14 09:26:00 +0100

    Deps update. default setting for showTitlebar to be false on non-darwin

--- src/shared/Models/Settings/UISettings.js
+++ src/shared/Models/Settings/UISettings.js
@@ -1,7 +1,7 @@
 const Model = require('../Model')
 
 class UISettings extends Model {
-  get showTitlebar () { return this._value_('showTitlebar', false) }
+  get showTitlebar () { return this._value_('showTitlebar', process.platform !== 'darwin') }
   get showAppBadge () { return this._value_('showAppBadge', true) }
   get showTitlebarCount () { return this._value_('showTitlebarCount', true) }
   get showAppMenu () { return this._value_('showAppMenu', process.platform !== 'win32') }
