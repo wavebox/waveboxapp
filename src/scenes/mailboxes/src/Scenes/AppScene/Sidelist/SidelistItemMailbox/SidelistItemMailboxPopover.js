061cfaf1f4a98fbea3b56e6c2668fea222dc9afe
Thomas Beverley <tom@locly.com>
2017-05-17 14:50:45 +0100

    Mailbox popover menu has option to move service up and down #173

--- src/scenes/mailboxes/src/Scenes/AppScene/Sidelist/SidelistItemMailbox/SidelistItemMailboxPopover.js
+++ src/scenes/mailboxes/src/Scenes/AppScene/Sidelist/SidelistItemMailbox/SidelistItemMailboxPopover.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { Popover, Menu, MenuItem, Divider, FontIcon } from 'material-ui'
-import { mailboxActions, mailboxDispatch } from 'stores/mailbox'
+import { mailboxActions, mailboxDispatch, MailboxReducer } from 'stores/mailbox'
 import shallowCompare from 'react-addons-shallow-compare'
 import * as Colors from 'material-ui/styles/colors'
+import CoreMailbox from 'shared/Models/Accounts/CoreMailbox'
 
 export default class SidelistItemMailboxPopover extends React.Component {
   /* **************************************************************************/
@@ -74,7 +75,7 @@ export default class SidelistItemMailboxPopover extends React.Component {
   /**
   * Moves this item up
   */
-  handleMoveUp = () => {
+  handleMoveAccountUp = () => {
     this.handleClosePopover(() => {
       mailboxActions.moveUp(this.props.mailbox.id)
     })
@@ -83,9 +84,29 @@ export default class SidelistItemMailboxPopover extends React.Component {
   /**
   * Moves this item down
   */
-  handleMoveDown = () => {
+  handleMoveAccountDown = () => {
     this.handleClosePopover(() => {
       mailboxActions.moveDown(this.props.mailbox.id)
+    })
+  }
+
+  /**
+  * Moves the given service down
+  * @param serviceType: the type of service
+  */
+  handleMoveServiceUp = (serviceType) => {
+    this.handleClosePopover(() => {
+      mailboxActions.reduce(this.props.mailbox.id, MailboxReducer.moveServiceUp, serviceType)
+    })
+  }
+
+  /**
+  * Moves the given service up
+  * @param serviceType: the type of service
+  */
+  handleMoveServiceDown = (serviceType) => {
+    this.handleClosePopover(() => {
+      mailboxActions.reduce(this.props.mailbox.id, MailboxReducer.moveServiceDown, serviceType)
     })
   }
 
@@ -123,91 +144,145 @@ export default class SidelistItemMailboxPopover extends React.Component {
   }
 
   /**
-  * Renders the menu items
+  * Renders the movement menu items
   * @param mailbox: the mailbox to render for
-  * @param isFirst: true if this is the first item
-  * @Param isLast: true if this is the last item
-  * @return array of jsx elements
+  * @param isMailboxFirst: true if this is the first mailbox item
+  * @param isMailboxLast: true if this is the last mailbox item
+  * @param serviceType: the type of mailbox opened over
+  * @return an array of menu items
   */
-  renderMenuItems (mailbox, isFirst, isLast) {
-    const menuItems = [
-      // Mailbox Info
-      mailbox.displayName ? (
-        <MenuItem
-          key='info'
-          primaryText={mailbox.displayName}
-          disabled />) : undefined,
+  renderMovementMenuItems (mailbox, isMailboxFirst, isMailboxLast, serviceType) {
+    const menuItems = []
 
-      // Error handling
-      mailbox.isAuthenticationInvalid ? (
-        <MenuItem
-          key='reauthenticate'
-          primaryText='Reauthenticate'
-          onClick={this.handleReauthenticate}
-          style={{ color: Colors.red600 }}
-          leftIcon={<FontIcon className='material-icons' style={{ color: Colors.red600 }}>error_outline</FontIcon>} />
-      ) : undefined,
-      mailbox.isAuthenticationInvalid ? (
-        <Divider key='div-errors' />
-      ) : undefined,
-
-      // Ordering controls
-      isFirst ? undefined : (
+    if (!isMailboxFirst) {
+      menuItems.push(
         <MenuItem
           key='moveup'
-          primaryText='Move Up'
-          onClick={this.handleMoveUp}
-          leftIcon={<FontIcon className='material-icons'>arrow_upward</FontIcon>} />),
-      isLast ? undefined : (
+          primaryText='Move Account Up'
+          onClick={this.handleMoveAccountUp}
+          leftIcon={<FontIcon className='material-icons'>arrow_upward</FontIcon>} />
+      )
+    }
+    if (!isMailboxLast) {
+      menuItems.push(
         <MenuItem
           key='movedown'
-          primaryText='Move Down'
-          onClick={this.handleMoveDown}
-          leftIcon={<FontIcon className='material-icons'>arrow_downward</FontIcon>} />),
-      isFirst && isLast ? undefined : (<Divider key='div-ordering' />),
+          primaryText='Move Account Down'
+          onClick={this.handleMoveAccountDown}
+          leftIcon={<FontIcon className='material-icons'>arrow_downward</FontIcon>} />
+      )
+    }
 
-      // Account Actions
-      (<MenuItem
-        key='delete'
-        primaryText='Delete'
-        onClick={this.handleDelete}
-        leftIcon={<FontIcon className='material-icons'>delete</FontIcon>} />),
-      (<MenuItem
-        key='settings'
-        primaryText='Account Settings'
-        onClick={this.handleAccountSettings}
-        leftIcon={<FontIcon className='material-icons'>settings</FontIcon>} />),
-      !mailbox.artificiallyPersistCookies ? undefined : (
-        <MenuItem
-          key='reauthenticate'
-          primaryText='Re-Authenticate'
-          onClick={this.handeReauthenticateBrowserSession}
-          leftIcon={<FontIcon className='material-icons'>lock_outline</FontIcon>} />),
-      (<Divider key='div-actions' />),
+    if (serviceType !== CoreMailbox.SERVICE_TYPES.DEFAULT && mailbox.additionalServiceTypes.length > 1) {
+      const serviceName = (mailbox.serviceForType(serviceType) || {}).humanizedType || 'Service'
+      const enabledServiceTypes = mailbox.additionalServiceTypes
+      const serviceIndex = enabledServiceTypes.findIndex((s) => s === serviceType)
+      const isServiceFirst = serviceIndex === 0
+      const isServiceLast = serviceIndex === enabledServiceTypes.length - 1
 
-      // Advanced Actions
-      (<MenuItem
-        key='reload'
-        primaryText='Reload'
-        onClick={this.handleReload}
-        leftIcon={<FontIcon className='material-icons'>refresh</FontIcon>} />),
-      (<MenuItem
-        key='sync'
-        primaryText='Resync'
-        onClick={this.handleResync}
-        leftIcon={<FontIcon className='material-icons'>sync</FontIcon>} />),
-      (<MenuItem
-        key='inspect'
-        primaryText='Inspect'
-        onClick={this.handleInspect}
-        leftIcon={<FontIcon className='material-icons'>bug_report</FontIcon>} />)
-    ].filter((item) => !!item)
+      if (!isServiceFirst) {
+        menuItems.push(
+          <MenuItem
+            key='moveup_service'
+            primaryText={`Move ${serviceName} Up`}
+            onClick={() => this.handleMoveServiceUp(serviceType)}
+            leftIcon={<FontIcon className='material-icons'>arrow_upward</FontIcon>} />
+        )
+      }
+      if (!isServiceLast) {
+        menuItems.push(
+          <MenuItem
+            key='movedown_service'
+            primaryText={`Move ${serviceName} Down`}
+            onClick={() => this.handleMoveServiceDown(serviceType)}
+            leftIcon={<FontIcon className='material-icons'>arrow_downward</FontIcon>} />
+        )
+      }
+    }
+
+    if (menuItems.length) {
+      menuItems.push(<Divider key='div-ordering' />)
+    }
+
+    return menuItems
+  }
+
+  /**
+  * Renders the menu items
+  * @param mailbox: the mailbox to render for
+  * @param isMailboxFirst: true if this is the first item
+  * @param isMailboxLast: true if this is the last item
+  * @param serviceType: the type of service the user clicked on
+  * @return array of jsx elements
+  */
+  renderMenuItems (mailbox, isMailboxFirst, isMailboxLast, serviceType) {
+    const menuItems = [].concat(
+      [
+        // Mailbox Info
+        mailbox.displayName ? (
+          <MenuItem
+            key='info'
+            primaryText={mailbox.displayName}
+            disabled />) : undefined,
+
+        // Error handling
+        mailbox.isAuthenticationInvalid ? (
+          <MenuItem
+            key='reauthenticate'
+            primaryText='Reauthenticate'
+            onClick={this.handleReauthenticate}
+            style={{ color: Colors.red600 }}
+            leftIcon={<FontIcon className='material-icons' style={{ color: Colors.red600 }}>error_outline</FontIcon>} />
+        ) : undefined,
+        mailbox.isAuthenticationInvalid ? (
+          <Divider key='div-errors' />
+        ) : undefined
+      ],
+      this.renderMovementMenuItems(mailbox, isMailboxFirst, isMailboxLast, serviceType),
+      [
+        // Account Actions
+        (<MenuItem
+          key='delete'
+          primaryText='Delete'
+          onClick={this.handleDelete}
+          leftIcon={<FontIcon className='material-icons'>delete</FontIcon>} />),
+        (<MenuItem
+          key='settings'
+          primaryText='Account Settings'
+          onClick={this.handleAccountSettings}
+          leftIcon={<FontIcon className='material-icons'>settings</FontIcon>} />),
+        !mailbox.artificiallyPersistCookies ? undefined : (
+          <MenuItem
+            key='reauthenticate'
+            primaryText='Re-Authenticate'
+            onClick={this.handeReauthenticateBrowserSession}
+            leftIcon={<FontIcon className='material-icons'>lock_outline</FontIcon>} />),
+        (<Divider key='div-actions' />),
+
+        // Advanced Actions
+        (<MenuItem
+          key='reload'
+          primaryText='Reload'
+          onClick={this.handleReload}
+          leftIcon={<FontIcon className='material-icons'>refresh</FontIcon>} />),
+        (<MenuItem
+          key='sync'
+          primaryText='Resync'
+          onClick={this.handleResync}
+          leftIcon={<FontIcon className='material-icons'>sync</FontIcon>} />),
+        (<MenuItem
+          key='inspect'
+          primaryText='Inspect'
+          onClick={this.handleInspect}
+          leftIcon={<FontIcon className='material-icons'>bug_report</FontIcon>} />)
+      ]
+    ).filter((item) => !!item)
 
     return menuItems
   }
 
   render () {
-    const { mailbox, isFirst, isLast, isOpen, anchor } = this.props
+    const { mailbox, isFirst, isLast, isOpen, anchor, serviceType } = this.props
 
     return (
       <Popover
@@ -217,7 +292,7 @@ export default class SidelistItemMailboxPopover extends React.Component {
         targetOrigin={{ horizontal: 'left', vertical: 'top' }}
         onRequestClose={this.handleClosePopover}>
         <Menu desktop onEscKeyDown={this.handleClosePopover}>
-          {this.renderMenuItems(mailbox, isFirst, isLast)}
+          {this.renderMenuItems(mailbox, isFirst, isLast, serviceType)}
         </Menu>
       </Popover>
     )
