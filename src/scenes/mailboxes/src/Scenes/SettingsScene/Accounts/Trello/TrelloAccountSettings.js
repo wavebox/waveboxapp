463e61b5174f5d8af5db6327362425e22210f544
Thomas Beverley <tom@locly.com>
2017-08-24 15:40:57 +0100

    Added home board option for trello #212

--- src/scenes/mailboxes/src/Scenes/SettingsScene/Accounts/Trello/TrelloAccountSettings.js
+++ src/scenes/mailboxes/src/Scenes/SettingsScene/Accounts/Trello/TrelloAccountSettings.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import shallowCompare from 'react-addons-shallow-compare'
-import { Paper } from 'material-ui'
+import { Paper, SelectField, MenuItem } from 'material-ui'
 import { Row, Col } from 'Components/Grid'
 import AccountAppearanceSettings from '../AccountAppearanceSettings'
 import AccountAdvancedSettings from '../AccountAdvancedSettings'
@@ -11,6 +11,7 @@ import styles from '../../CommonSettingStyles'
 import CoreMailbox from 'shared/Models/Accounts/CoreMailbox'
 import AccountCustomCodeSettings from '../AccountCustomCodeSettings'
 import AccountBehaviourSettings from '../AccountBehaviourSettings'
+import { mailboxActions, TrelloDefaultServiceReducer } from 'stores/mailbox'
 
 export default class TrelloAccountSettings extends React.Component {
   /* **************************************************************************/
@@ -44,8 +45,26 @@ export default class TrelloAccountSettings extends React.Component {
           </Col>
           <Col md={6}>
             <Paper zDepth={1} style={styles.paper}>
+              <SelectField
+                fullWidth
+                floatingLabelText='Home board (opens on launch)'
+                value={service.homeBoardId || 'default'}
+                onChange={(evt, index, boardId) => {
+                  mailboxActions.reduceService(
+                    mailbox.id,
+                    service.type,
+                    TrelloDefaultServiceReducer.setHomeBoardId,
+                    boardId === 'default' ? undefined : boardId
+                  )
+                }}>
+                <MenuItem value={'default'} primaryText='Trello Home (Default)' />
+                {Array.from(service.boards).map((board) => {
+                  return (
+                    <MenuItem key={board.id} value={board.id} primaryText={board.name} />
+                  )
+                })}
+              </SelectField>
               <AccountBehaviourSettings mailbox={mailbox} service={service} />
-              <br />
               <AccountCustomCodeSettings
                 mailbox={mailbox}
                 service={service}
--- src/scenes/mailboxes/src/stores/mailbox/TrelloDefaultServiceReducer.js
+++ src/scenes/mailboxes/src/stores/mailbox/TrelloDefaultServiceReducer.js
@@ -14,6 +14,30 @@ class TrelloDefaultServiceReducer extends ServiceReducer {
       })
     })
   }
+
+  /**
+  * Sets the board info for this account
+  * @param mailbox: the mailbox to update
+  * @param service: the service to update
+  * @param boards: the boards to set
+  */
+  static setBoards (mailbox, service, boards) {
+    return service.changeData({
+      boards: boards
+    })
+  }
+
+  /**
+  * Sets the open board id
+  * @param mailbox: the mailbox to update
+  * @param service: the service to update
+  * @param boards: the boardId to open with
+  */
+  static setHomeBoardId (mailbox, service, boardId) {
+    return service.changeData({
+      homeBoardId: boardId
+    })
+  }
 }
 
 export default TrelloDefaultServiceReducer
