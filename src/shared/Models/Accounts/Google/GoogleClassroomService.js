193a170e863dac255dc77f7ed50c72d7b50498a1
Nathan Withers <nathanwithz@gmail.com>
2017-11-10 01:27:04 -0500

    Begin adding support for Google Classroom

---
+++ src/shared/Models/Accounts/Google/GoogleClassroomService.js
@@ -0,0 +1,27 @@
+const GoogleService = require('./GoogleService')
+
+class GoogleNotesService extends GoogleService {
+  /* **************************************************************************/
+  // Class
+  /* **************************************************************************/
+
+  static get type () { return GoogleService.SERVICE_TYPES.CLASSROOM }
+  static get humanizedType () { return 'Google Classroom' }
+  static get humanizedTypeShort () { return 'Classroom' }
+  static get humanizedLogos () {
+      /* TODO try to find 128px of icon*/
+    return [
+      'images/google/logo_classroom_32px.png',
+      'images/google/logo_classroom_48px.png',
+      'images/google/logo_classroom_64px.png'
+    ]
+  }
+
+  /* **************************************************************************/
+  // Properties
+  /* **************************************************************************/
+
+  get url () { return 'https://classroom.google.com' }
+}
+
+module.exports = GoogleClassroomService
