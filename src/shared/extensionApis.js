7971f72ff662c2c96d4ccea224f63bcc186bf68f
Thomas Beverley <tom@wavebox.io>
2018-03-12 10:19:06 +0000

    Fix linkter

--- src/shared/extensionApis.js
+++ src/shared/extensionApis.js
@@ -5,7 +5,7 @@ const WAVEBOX_CONTENT_IMPL_ENDPOINTS = {
   WINDOW_DIALOGS: 'NativeWindowDialogs.js',
 
   ONEDRIVE_WINDOW_OPEN: 'OnedriveWindowOpen.js',
-  GOOGLE_MAIL_WINDOW_OPEN: 'GoogleMailWindowOpen.js',
+  GOOGLE_MAIL_WINDOW_OPEN: 'GoogleMailWindowOpen.js'
 }
 const VALID_WAVEBOX_CONTENT_IMPL_ENDPOINTS = new Set(Array.from(Object.keys(WAVEBOX_CONTENT_IMPL_ENDPOINTS).map((k) => WAVEBOX_CONTENT_IMPL_ENDPOINTS[k])))
 
