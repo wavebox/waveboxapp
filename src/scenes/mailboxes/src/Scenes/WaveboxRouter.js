d370f7a6f2c8dc67fb0d5b006b7056f823d4cbde
Thomas Beverley <tom@wavebox.io>
2018-05-21 16:32:13 +0100

    Add an error boundary into the main router so we don't completely die

--- src/scenes/mailboxes/src/Scenes/WaveboxRouter.js
+++ src/scenes/mailboxes/src/Scenes/WaveboxRouter.js
@@ -29,6 +29,7 @@ import {
   AccountAuthenticatingScene,
   AccountStandaloneScene
 } from './AccountScene'
+import WaveboxRouterErrorBoundary from './WaveboxRouterErrorBoundary'
 
 export default class WaveboxRouter extends React.Component {
   /* **************************************************************************/
@@ -46,35 +47,37 @@ export default class WaveboxRouter extends React.Component {
           <EarlyBuildToast />
           <NotificationPanel />
 
-          <Route path='/settings/:tab?/:tabArg?' component={SettingsScene} />
-          <Route path='/dictionary_installer' component={DictionaryInstallerScene} />
+          <WaveboxRouterErrorBoundary>
+            <Route path='/settings/:tab?/:tabArg?' component={SettingsScene} />
+            <Route path='/dictionary_installer' component={DictionaryInstallerScene} />
 
-          <Route path='/mailbox_wizard/add' component={MailboxWizardAddScene} />
-          <Route path='/mailbox_wizard/:mailboxType/:accessMode/:step/:mailboxId?' component={MailboxWizardScene} />
-          <Route path='/mailbox/reauthenticating' component={MailboxReauthenticatingScene} />
+            <Route path='/mailbox_wizard/add' component={MailboxWizardAddScene} />
+            <Route path='/mailbox_wizard/:mailboxType/:accessMode/:step/:mailboxId?' component={MailboxWizardScene} />
+            <Route path='/mailbox/reauthenticating' component={MailboxReauthenticatingScene} />
 
-          <Route path='/mailbox_delete/:mailboxId' component={MailboxDeleteScene} />
-          <Route path='/mailbox_service_delete/:mailboxId/:serviceType' component={MailboxServiceDeleteScene} />
+            <Route path='/mailbox_delete/:mailboxId' component={MailboxDeleteScene} />
+            <Route path='/mailbox_service_delete/:mailboxId/:serviceType' component={MailboxServiceDeleteScene} />
 
-          <Route path='/app_wizard/:step?' component={AppWizardScene} />
+            <Route path='/app_wizard/:step?' component={AppWizardScene} />
 
-          <Route path='/incoming/compose' component={ComposePickerScene} />
+            <Route path='/incoming/compose' component={ComposePickerScene} />
 
-          <Route path='/updates/checking/:provider' component={CheckingUpdatesScene} />
-          <Route path='/updates/none/:provider' component={UpdateNoneScene} />
-          <Route path='/updates/error/:provider' component={UpdateErrorScene} />
-          <Route path='/updates/install/:provider' component={UpdateAvailableScene} />
-          <Route path='/updates/available/:provider' component={UpdateAvailableScene} />
+            <Route path='/updates/checking/:provider' component={CheckingUpdatesScene} />
+            <Route path='/updates/none/:provider' component={UpdateNoneScene} />
+            <Route path='/updates/error/:provider' component={UpdateErrorScene} />
+            <Route path='/updates/install/:provider' component={UpdateAvailableScene} />
+            <Route path='/updates/available/:provider' component={UpdateAvailableScene} />
 
-          <Route path='/pro' component={ProScene} />
-          <Route path='/news' component={NewsScene} />
+            <Route path='/pro' component={ProScene} />
+            <Route path='/news' component={NewsScene} />
 
-          <Route path='/account/message' component={AccountMessageScene} />
-          <Route path='/account/auth/:mode?' component={AccountAuthScene} />
-          <Route path='/account/authenticating' component={AccountAuthenticatingScene} />
-          <Route path='/account/view' component={AccountStandaloneScene} />
+            <Route path='/account/message' component={AccountMessageScene} />
+            <Route path='/account/auth/:mode?' component={AccountAuthScene} />
+            <Route path='/account/authenticating' component={AccountAuthenticatingScene} />
+            <Route path='/account/view' component={AccountStandaloneScene} />
 
-          <Route path='/setup/linux' component={LinuxSetupScene} />
+            <Route path='/setup/linux' component={LinuxSetupScene} />
+          </WaveboxRouterErrorBoundary>
           <PrivacyDialog />
         </div>
       </HashRouter>
