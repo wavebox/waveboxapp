4baaf0c6ad3d8da50d21b590b73a4920263e9738
Thomas Beverley <tom@wavebox.io>
2018-07-10 13:49:29 +0100

    Style fix

--- src/scenes/wbui/AuthenticationInstruction.js
+++ src/scenes/wbui/AuthenticationInstruction.js
@@ -9,6 +9,8 @@ import lightBlue from '@material-ui/core/colors/lightBlue'
 
 const styles = {
   container: {
+    width: '100%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
