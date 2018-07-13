f08ab2953fe5b16e72cb1fb5322a380c7415e126
Thomas Beverley <tom@wavebox.io>
2018-07-13 14:07:44 +0100

    Fix duplicate print buttons

--- src/app/src/Services/PDFRenderService.js
+++ src/app/src/Services/PDFRenderService.js
@@ -85,6 +85,9 @@ class PDFRenderService {
   _injectPrintButton (wc) {
     wc.executeJavaScript(`
       ;(function () {
+        const toolbar = document.querySelector('#toolbar').shadowRoot
+        if (toolbar.getElementById('print')) { return }
+
         const printButton = document.createElement('paper-icon-button')
         printButton.id = 'print'
         printButton.setAttribute('icon', 'cr:print')
@@ -93,7 +96,7 @@ class PDFRenderService {
         printButton.addEventListener('click', () => {
           document.title = 'wbaction:print'
         })
-        const toolbar = document.querySelector('#toolbar').shadowRoot
+
         const downloadButton = toolbar.querySelector('#download')
         downloadButton.parentElement.insertBefore(printButton, downloadButton.nextSibling)
       })()
