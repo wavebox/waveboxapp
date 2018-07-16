caaccfe62089123e80aa4acff8632141538ebf74
Thomas Beverley <tom@wavebox.io>
2018-07-16 10:13:24 +0100

    Re-enable package-lock with the latest version of npm

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 Your client for Gmail, Inbox, Outlook, O365, Trello, Slack & more!
 
-Wavebox is the clever new home for cloud apps on macOS, Linux & Windows bringing  Gmail, Inbox, Outlook, O365, Trello, Slack & more into a configurable client.  
+Wavebox is the clever new home for cloud apps on macOS, Linux & Windows bringing  Gmail, Inbox, Outlook, O365, Trello, Slack & more into a configurable client.
 
 Gone are the days of opening countless browser tabs and logging in and out of your favourite cloud accounts. Now you can launch Wavebox with one click, and instantly access them all in one UI, and without slowing your machine.
 
@@ -44,6 +44,7 @@ Before you get started you'll need the following
 
 * Python 2.7
 * Nodejs 8.9.3
+* npm >= 6.2.0
 * Wavebox API Key
   * Download & install Wavebox for free
   * Go to Settings and then the Wavebox Pro Tab
--- scripts/auditAll.js
+++ scripts/auditAll.js
@@ -1,22 +1,11 @@
 const { PACKAGE_DIRS } = require('./constants')
 const { promiseSpawn } = require('./Tools')
 const Colors = require('colors/safe')
-const fs = require('fs-extra')
-const path = require('path')
 
 Promise.resolve()
   .then(() => {
     return PACKAGE_DIRS.reduce((acc, dir) => {
       return acc
-        .then(() => fs.move(path.join(dir, '.npmrc'), path.join(dir, '.npmrc.bak')))
-        .then(() => {
-          console.log(`${Colors.inverse('npm install:')} ${dir}`)
-          return promiseSpawn(
-            process.platform === 'win32' ? 'npm.cmd' : 'npm',
-            ['install', '--package-lock-only'],
-            { cwd: dir }
-          )
-        })
         .then(() => {
           console.log(`${Colors.inverse('npm audit:')} ${dir}`)
           return promiseSpawn(
@@ -25,11 +14,8 @@ Promise.resolve()
             { stdio: 'inherit', cwd: dir }
           ).catch(() => Promise.resolve()) // Audit returns an error code for failed audits
         })
-        .then(() => fs.move(path.join(dir, '.npmrc.bak'), path.join(dir, '.npmrc')))
-        .then(() => fs.remove(path.join(dir, 'package-lock.json')))
     }, Promise.resolve())
   })
   .catch((e) => {
-    console.log(Colors.bgRed.white('audit:all failed and probably left your install in a dirty state. You should run git status to fix'))
     process.exit(-1)
   })
