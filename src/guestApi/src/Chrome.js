98bf433e5052426fb649f612fdc5d3e20127707e
Thomas Beverley <tom@wavebox.io>
2019-02-08 12:57:07 +0000

    Patch Google Drive

--- src/guestApi/src/Chrome.js
+++ src/guestApi/src/Chrome.js
@@ -3,6 +3,29 @@
 ;(function () {
   window.chrome = {}
 
+  if (window.location.hostname === 'drive.google.com') {
+    window.chrome.runtime = {
+      connect: function (extensionId, args) {
+        return {
+          name: (args || {}).name,
+          sender: undefined,
+          postMessage: function () { throw new Error('Attempting to use a disconnected port object') },
+          disconnect: function () { },
+          ...['onDisconnect', 'onMessage'].reduce((acc, k) => {
+            return {
+              addListener: function () { },
+              dispatch: function () { },
+              hasListener: function () { },
+              hasListeners: function () { },
+              removeListener: function () { },
+              removeAllListenrs: function () { }
+            }
+          }, {})
+        }
+      }
+    }
+  }
+
   if (window.location.hostname === 'docs.google.com') {
     window._docs_chrome_extension_exists = true
     window._docs_chrome_extension_permissions = [
