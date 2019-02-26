d74a5e1d77851d1a43f1582d451a1c4bc30eb595
Thomas Beverley <tom@wavebox.io>
2019-02-26 13:30:27 +0000

    Tweak to support screen

--- src/scenes/mailboxes/src/Scenes/SettingsScene/SupportSettings/SupportSettings.js
+++ src/scenes/mailboxes/src/Scenes/SettingsScene/SupportSettings/SupportSettings.js
@@ -91,9 +91,6 @@ const styles = {
   contentContainer: {
     width: '100%',
     height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
     textAlign: 'center'
   },
   titleUnboxed: {
