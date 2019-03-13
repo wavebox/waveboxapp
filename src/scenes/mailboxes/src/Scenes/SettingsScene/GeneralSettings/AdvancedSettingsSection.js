142ec1aeaea57109954a5a9f4d51b01f038c53e6
Thomas Beverley <tom@wavebox.io>
2019-03-13 15:25:19 +0000

    Separate service and process sandboxing more clearly

--- src/scenes/mailboxes/src/Scenes/SettingsScene/GeneralSettings/AdvancedSettingsSection.js
+++ src/scenes/mailboxes/src/Scenes/SettingsScene/GeneralSettings/AdvancedSettingsSection.js
@@ -122,7 +122,7 @@ class AdvancedSettingsSection extends React.Component {
           checked={app.isolateMailboxProcesses} />
         {AppSettings.SUPPORTS_MIXED_SANDBOX_MODE ? (
           <SettingsListItemSwitch
-            label='Sandboxing (Requires Restart)'
+            label='Process Sandboxing (Requires Restart)'
             onChange={(evt, toggled) => {
               showRestart()
               settingsActions.sub.app.setEnableMixedSandboxMode(toggled)
