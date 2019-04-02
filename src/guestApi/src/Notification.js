9c3ac0ba4f15d022d2058b321b70cbed3719d8dc
Thomas Beverley <tom@wavebox.io>
2019-04-02 09:19:29 +0100

    Fix notifications not switching to the correct item in Microsoft Teams

--- src/guestApi/src/Notification.js
+++ src/guestApi/src/Notification.js
@@ -3,6 +3,14 @@
 /* global WB_CONFIG */
 
 ;(function () {
+  const privId = Symbol('privId')
+  const privTitle = Symbol('privTitle')
+  const privOptions = Symbol('privOptions')
+  const privCreated = Symbol('privCreated')
+  const privEventHandlers = Symbol('privEventHandlers')
+  const privOnClick = Symbol('privOnClick')
+  const privOnError = Symbol('privOnError')
+
   const openNotifications = []
   const permissionRequests = new Map()
   let idAcc = 0
@@ -17,12 +25,12 @@
       if (!data.wavebox) { return }
 
       if (data.type === 'wavebox-notification-clicked') {
-        const notificationIndex = openNotifications.findIndex((n) => n.__id__ === data.notificationId)
+        const notificationIndex = openNotifications.findIndex((n) => n[privId] === data.notificationId)
         if (notificationIndex !== -1) {
           const notification = openNotifications[notificationIndex]
           openNotifications.splice(notificationIndex, 1)
 
-          const clickHandlers = notification.__eventHandlers__
+          const clickHandlers = notification[privEventHandlers]
             .filter((handler) => handler.type === 'click')
             .map((handler) => handler.listener)
             .concat(notification.onclick ? [notification.onclick] : [])
@@ -44,18 +52,18 @@
               timeStamp: new Date().getTime(),
               type: 'click'
             }
-            const args = [event]
+
             clickHandlers.forEach((handler) => {
               if (typeof (handler) === 'function') {
-                handler.apply(notification, args)
+                handler.apply(notification, [event])
               } else if (typeof (handler) === 'object' && typeof (handler.handleEvent) === 'function') {
-                handler.handleEvent.apply(notification, args)
+                handler.handleEvent(event)
               }
             })
           }
         }
       } else if (data.type === 'wavebox-notification-closed') {
-        const notificationIndex = openNotifications.findIndex((n) => n.__id__ === data.notificationId)
+        const notificationIndex = openNotifications.findIndex((n) => n[privId] === data.notificationId)
         if (notificationIndex !== -1) {
           openNotifications.splice(notificationIndex, 1)
         }
@@ -110,19 +118,19 @@
     */
     constructor (title, options = {}) {
       idAcc += 1
-      this.__id__ = idAcc
-      this.__title__ = title
-      this.__options__ = Object.freeze(Object.assign({}, options))
-      this.__created__ = new Date().getTime()
-      this.__eventHandlers__ = []
-      this.__onclick__ = null
-      this.__onerror__ = null
+      this[privId] = idAcc
+      this[privTitle] = title
+      this[privOptions] = Object.freeze({ ...options })
+      this[privCreated] = new Date().getTime()
+      this[privEventHandlers] = []
+      this[privOnClick] = null
+      this[privOnError] = null
 
       window.top.postMessage(JSON.stringify({
         apiKey: WB_API_KEY,
         wavebox: true,
         type: 'wavebox-notification-present',
-        notificationId: this.__id__,
+        notificationId: this[privId],
         notification: {
           title: title,
           options: options
@@ -136,25 +144,25 @@
     // Properties
     /* **************************************************************************/
 
-    get actions () { return this.__options__.actions }
-    get badge () { return this.__options__.badge }
-    get body () { return this.__options__.body }
-    get data () { return this.__options__.data }
-    get dir () { return this.__options__.dir }
-    get lang () { return this.__options__.lang }
-    get tag () { return this.__options__.tag }
-    get icon () { return this.__options__.icon }
-    get image () { return this.__options__.image }
-    get requireInteraction () { return this.__options__.requireInteraction }
-    get silent () { return this.__options__.silent }
-    get timestamp () { return this.__options__.timestamp === undefined ? this.__created__ : this.__options__.timestamp }
-    get title () { return this.__options__.title }
-    get vibrate () { return this.__options__.vibrate ? [1] : [0] }
+    get actions () { return this[privOptions].actions }
+    get badge () { return this[privOptions].badge }
+    get body () { return this[privOptions].body }
+    get data () { return this[privOptions].data }
+    get dir () { return this[privOptions].dir }
+    get lang () { return this[privOptions].lang }
+    get tag () { return this[privOptions].tag }
+    get icon () { return this[privOptions].icon }
+    get image () { return this[privOptions].image }
+    get requireInteraction () { return this[privOptions].requireInteraction }
+    get silent () { return this[privOptions].silent }
+    get timestamp () { return this[privOptions].timestamp === undefined ? this[privCreated] : this[privOptions].timestamp }
+    get title () { return this[privTitle] }
+    get vibrate () { return this[privOptions].vibrate ? [1] : [0] }
 
-    get onclick () { return this.__onclick__ }
-    set onclick (v) { this.__onclick__ = v }
-    get onerror () { return this.__onerror__ }
-    set onerror (v) { this.__onerror__ = v }
+    get onclick () { return this[privOnClick] }
+    set onclick (v) { this[privOnClick] = v }
+    get onerror () { return this[privOnError] }
+    set onerror (v) { this[privOnError] = v }
 
     /* **************************************************************************/
     // Methods
@@ -165,7 +173,7 @@
         apiKey: WB_API_KEY,
         wavebox: true,
         type: 'browser-notification-close',
-        notificationId: this.__id__
+        notificationId: this[privId]
       }), '*')
     }
 
@@ -176,7 +184,7 @@
     * @param useCapture: ignored
     */
     addEventListener (type, listener, useCapture) {
-      this.__eventHandlers__.push({ type: type, listener: listener })
+      this[privEventHandlers].push({ type: type, listener: listener })
     }
 
     /**
@@ -185,7 +193,7 @@
     * @param listener: the listener callback or event handler
     */
     removeEventListener (type, listener) {
-      this.__eventHandlers__ = this.__eventHandlers__.filter((r) => r.type !== type && r.listener !== listener)
+      this[privEventHandlers] = this[privEventHandlers].filter((r) => r.type !== type && r.listener !== listener)
     }
   }
 
